be08d360b80c0e00d858492b9ac5167204422f6e
yashu55 <ysp551996@gmail.com>
2021-03-27 12:46:59 +0530

    pulled changes from master

--- src/main/java/com/bic/dto/CustomerRegisterStatus.java
+++ src/main/java/com/bic/dto/CustomerRegisterStatus.java
@@ -2,14 +2,14 @@ package com.bic.dto;
 
 public class CustomerRegisterStatus extends Status {
 
-	private int registeredCustomerId;
+    private int registeredCustomerId;
 
-	public int getRegisteredCustomerId() {
-		return registeredCustomerId;
-	}
+    public int getRegisteredCustomerId() {
+	return registeredCustomerId;
+    }
 
-	public void setRegisteredCustomerId(int registeredCustomerId) {
-		this.registeredCustomerId = registeredCustomerId;
-	}
-	//test comment
+    public void setRegisteredCustomerId(int registeredCustomerId) {
+	this.registeredCustomerId = registeredCustomerId;
+    }
+
 }
