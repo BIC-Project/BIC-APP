0292f09d24a7832048287f41af099f85af1cd9fc
yashu55 <ysp551996@gmail.com>
2021-04-01 19:26:07 +0530

    Validations Done

--- src/main/java/com/bic/controller/ReceiptController.java
+++ src/main/java/com/bic/controller/ReceiptController.java
@@ -1,37 +1,87 @@
 package com.bic.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bic.dto.ReceiptCRUDStatus;
 import com.bic.dto.ReceiptGetAllStatus;
 import com.bic.dto.Status.StatusType;
+import com.bic.dto.ValidationErrorStatus;
 import com.bic.entity.Receipt;
 import com.bic.service.ReceiptService;
 
 @RestController
 @CrossOrigin
+@Validated
 public class ReceiptController {
 
 	@Autowired
 	private ReceiptService receiptService;
 
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationErrorStatus> handleValidationExceptions(
+			MethodArgumentNotValidException e) {
+		ValidationErrorStatus status = new ValidationErrorStatus();
+		status.setStatus(StatusType.FAILURE);
+		status.setMessage("Validation Error");
+		Map<String, String> errors = new HashMap<>();
+		e.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		// status.setErrors(errors);
+		return new ResponseEntity<ValidationErrorStatus>(status,
+				HttpStatus.BAD_REQUEST);
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<ValidationErrorStatus> handleConstraintViolationException(
+			ConstraintViolationException e) {
+		ValidationErrorStatus status = new ValidationErrorStatus();
+		status.setStatus(StatusType.FAILURE);
+		status.setMessage("Validation Error");
+		Map<String, String> errors = new HashMap<>();
+		e.getConstraintViolations().forEach((error) -> {
+			String fieldName = error.getPropertyPath().toString();
+			String errorMessage = error.getMessageTemplate();
+			errors.put(fieldName, errorMessage);
+		});
+
+		status.setErrors(errors);
+		return new ResponseEntity<ValidationErrorStatus>(status,
+				HttpStatus.BAD_REQUEST);
+	}
+
 	@PostMapping("/receipt")
 	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 	public ResponseEntity<ReceiptCRUDStatus> saveReceipt(
-			@RequestBody Receipt receipt) {
+			@Validated @RequestBody Receipt receipt) {
 
 		try {
 			Receipt outputReceipt = receiptService.saveReceipt(receipt);
@@ -49,13 +99,6 @@ public class ReceiptController {
 			return new ResponseEntity<ReceiptCRUDStatus>(status,
 					HttpStatus.CONFLICT);
 		}
-	}
-
-	@GetMapping("/receipt/{receiptId}")
-	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-	public ResponseEntity<ReceiptGetAllStatus> getReceipt() {
-
-		return null;
 	}
 
 	@DeleteMapping("/receipt/{receiptId}")
@@ -86,7 +129,7 @@ public class ReceiptController {
 	@GetMapping("/receipt")
 	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 	public ResponseEntity<ReceiptGetAllStatus> receiptList(
-			@RequestParam("receiptType") String receiptType,
+			@RequestParam("receiptType") @Pattern(regexp = "^[ED]R$", message = "Invalid receipt type selected. A valid receipt type is ER or DR.") @NotBlank(message = "Receipt type cannot be blank.") String receiptType,
 			@RequestParam(name = "customerId", required = false) String customerId,
 			@RequestParam("fromDateTime") String fromDateTime,
 			@RequestParam("toDateTime") String toDateTime,
--- src/main/java/com/bic/entity/Customer.java
+++ src/main/java/com/bic/entity/Customer.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,33 +25,48 @@ import lombok.Setter;
 @Table(name = "CUSTOMER_TBL")
 public class Customer {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int customerId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 
-    @Column(nullable = false, columnDefinition = "varchar(30)", unique = true)
-    private String customerName;
+	private int customerId;
 
-    @Column(nullable = false, columnDefinition = "varchar(200)")
-    private String customerAddress;
+	@Column(nullable = false, columnDefinition = "varchar(30)", unique = true)
+	@NotBlank(message = "Customer name cannot be empty.")
+	@Size(min = 3, max = 30, message = "Customer name should be between 3 to 30 characters.")
+	private String customerName;
 
-    @Column(nullable = false, columnDefinition = "varchar(7)")
-    private String customerPinCode;
+	@Column(nullable = false, columnDefinition = "varchar(200)")
+	@NotBlank(message = "Customer address cannot be empty.")
+	@Size(min = 3, max = 200, message = "Customer address should be between 3 to 200 characters.")
+	private String customerAddress;
 
-    @Column(nullable = false, columnDefinition = "varchar(30)")
-    private String contactName1;
+	@Column(nullable = false, columnDefinition = "varchar(7)")
+	@Pattern(regexp = "^[1-9][0-9]{5}$", message = "Pin Code should be 6 digits. Ex. 416416")
+	@NotBlank(message = "Pincode cannot be empty.")
+	private String customerPinCode;
 
-    @Column(nullable = false, columnDefinition = "varchar(30)")
-    private String contactName2;
+	@Column(nullable = false, columnDefinition = "varchar(30)")
+	@NotBlank(message = "Contact person name cannot be empty.")
+	@Size(min = 3, max = 30, message = "Contact person name should be between 3 to 30 characters.")
+	private String contactName1;
 
-    @Column(nullable = false, columnDefinition = "varchar(10)")
-    private String contactNumber1;
+	@Column(nullable = true, columnDefinition = "varchar(30)")
+	@Size(min = 3, max = 30, message = "Contact person name should be between 3 to 30 characters.")
+	private String contactName2;
 
-    @Column(nullable = false, columnDefinition = "varchar(10)")
-    private String contactNumber2;
+	@Column(nullable = false, columnDefinition = "varchar(10)")
+	@NotBlank(message = "Contact person number cannot be empty.")
+	@Pattern(regexp = "^[0-9]{3,10}$", message = "Contact number should only contain digits. Ex. 91XXXXXXXX")
+	@Size(min = 3, max = 10, message = "Contact number should be between 3 and 10 digits.")
+	private String contactNumber1;
 
-    @Column(nullable = false)
-    private boolean isActive;
+	@Column(nullable = true, columnDefinition = "varchar(10)")
+	@Pattern(regexp = "^[0-9]{3,10}$", message = "Contact number should only contain digits. Ex. 91XXXXXXXX")
+	@Size(min = 3, max = 10, message = "Contact number should be between 3 and 10 digits.")
+	private String contactNumber2;
+
+	@Column(nullable = false)
+	private boolean isActive;
 
 	public int getCustomerId() {
 		return customerId;
@@ -121,6 +139,5 @@ public class Customer {
 	public void setActive(boolean isActive) {
 		this.isActive = isActive;
 	}
-    
-    	
+
 }
